a7839538998b8d63f5c1ea5089c84239155cb372
gregroyclark <gregroyclark@gmail.com>
2020-12-29 11:04:21 -0600

    defines & returns searchTerm

--- js/dataFunctions.js
+++ js/dataFunctions.js
@@ -1,4 +1,6 @@
 export const getSearchTerm = () => {
     const rawSearchTerm = document.getElementById("search").value.trim();
     const regex = /[ ]{2,}/gi;
+    const searchTerm = rawSearchTerm.replaceAll(regex, " ");
+    return searchTerm;
 }
