3d1aa9653de7238f5994576662d2c4eb3669b175
gregroyclark <gregroyclark@gmail.com>
2020-12-29 03:14:20 -0600

    sets up initApp function

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@ document.addEventListener("readystatechange", (event) => {
     if (event.target.readyState === "complete") {
         initApp()
     }
-} )
+});
+
+const initApp = () => {
+    // set the focus
+
+    // 3 listeners
+
+    const form = document.getElementById("searchBar");
+    form.addEventListener("submit", submitTheSearch);
+}
