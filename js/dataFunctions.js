a0983650188ae229a4c7781f13a4aa102392e5a3
gregroyclark <gregroyclark@gmail.com>
2020-12-29 12:30:53 -0600

    adds getMaxChars function

--- js/dataFunctions.js
+++ js/dataFunctions.js
@@ -12,3 +12,12 @@ export const retrieveSearchResults = async (searchTerm) => {
 export const getWikiSearchString = (searchTerm) => {
     const maxChars = getMaxChars();
 }
+
+const getMaxChars = () => {
+    const width = window.innerWidth || document.body.clientWidth;
+    let maxChars;
+    if (width < 414) maxChars = 65;
+    if (width >= 414 && width < 1400) maxChars = 100;
+    if (width >= 1400) maxChars = 130;
+    return maxChars
+}
